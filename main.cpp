d273f7d6a386ae934864fd73dbd7d1f6826c7c71
jewcomm <navr.a@yandex.ru>
2022-11-06 18:01:31 +0300

    This work with threading.. but very slow(

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <cstring>
 #include <vector>
 #include <ctime>
+#include <chrono>
+#include <thread>
 
 /*
   Name  : CRC-32
@@ -15,6 +17,8 @@
   MaxLen: 268 435 455 байт (2 147 483 647 бит)
 */
 
+#define PURE_PASS false
+
 uint32_t crcPoly = 0xEDB88320UL;
 uint32_t crcTable[256];
 
@@ -25,6 +29,10 @@ std::vector<unsigned char> allSymbols;
 unsigned char * pass;
 size_t passLen = 6;
 uint32_t passCRC;
+
+// глобальная переменная для завершения потоков, если нашли пароль
+bool passFounded = false;
+
 
 void crc32TableGen(){
     uint32_t crc;
@@ -47,44 +55,61 @@ uint32_t crc32Func(char *buf, uint32_t len){
 
 unsigned char *passwordGen(size_t len){
     auto *password = new unsigned char[len + 1];
-    for(int i = 0; i < len; i++){
+    for(size_t i = 0; i < len; i++){
         password[i] = allSymbols.at(std::rand() % allSymbols.size()); // NOLINT(cert-msc30-c, cert-msc50-cpp)
     }
     password[len] = '\0';
     return password;
 }
 
-unsigned char * bruteForce6(size_t len, int beg, int last){
-    std::vector<int> symbPos;
-    symbPos.resize(len , 0);
-
+// 31513 ms
+void bruteForceU(size_t len, unsigned char start, unsigned char end){
     auto *val = new unsigned char[len + 1];
     val[len] = '\0';
 
-    symbPos[0] = beg;
-    while(symbPos[0] <= last){
-        if(symbPos[0] == allSymbols.size()) break;
+    for(size_t i = 1; i < len; i++) val[i] = allSymbols[0];
 
-        for(int i = symbPos.size() - 1; i > 0; i--){
-            if(symbPos[i] == allSymbols.size()){
-                symbPos[i] = 0;
-                symbPos[i - 1]++;
+    val[0] = start;
+
+    while(val[0] <= end && !passFounded){
+        for(int i = 0; i < allSymbols.size(); i++){
+            val[len - 1] = allSymbols[i];
+#if PURE_PASS
+            if(strncmp((char*)val, (char*)pass, len) == 0){
+                passFounded = true;
+                std::cout << val << std::endl;
+                return;
             }
+#else
+            if(passCRC == crc32Func((char*)val, len)){
+                std::cout << std::dec << "Founded pass: " << val << std::endl;
+                std::cout << std::hex << "CRC founded pass: " << crc32Func((char*)val, len) << std::endl;
+            }
+#endif
         }
-
-        for(int i = 0; i < len; i++){
-            val[i] = allSymbols[symbPos[i]];
+        // костыль, надо переделать
+        for(int i = len - 2; i >= 0; i--){
+            if(val[i] < allSymbols.back()) {
+                val[i]++;
+                break;
+            }
+            if(i != 0) val[i] = allSymbols.front();
+            else val[i]++;
         }
+    }
+    return;
+}
 
-        if(strncmp((char*)val, (char*)pass, len) == 0){
-            std::cout << val << std::endl;
-        }
-
-        if(/*val[1] == '!' && */val[2] == '!' && val[3] == '!' && val[4] == '!' && val[5] == '!'){
-            std::cout << val << std::endl;
-        }
-
-        symbPos[len - 1]++;
+void mThreadsRun(size_t count){
+    size_t step = allSymbols.size() / count;
+    std::vector<std::thread> threads;
+    threads.push_back(std::thread(bruteForceU, passLen, allSymbols.front(), allSymbols.at(step)));
+    for(size_t i = 1; i < count - 1; i++){
+        threads.push_back(std::thread(bruteForceU, passLen, allSymbols.at(i * step + 1), allSymbols.at((i + 1) * step)));
+    }
+    threads.push_back(std::thread(bruteForceU, passLen, allSymbols.at((count - 1) * step + 1), allSymbols.back()));
+    for(size_t i = 0 ; i < threads.size(); i++){
+        threads.at(i).join();
     }
 }
 
@@ -103,7 +128,13 @@ int main() {
     passCRC = crc32Func((char*)pass, passLen);
     std::cout << "CRC32 password: " << std::hex << passCRC << std::endl;
 
-    bruteForce6(6, 0, 2);
+    auto begin = std::chrono::steady_clock::now();
+
+    mThreadsRun(6);
+    auto end = std::chrono::steady_clock::now();
+
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    std::cout << std::dec << "The time: " << elapsed_ms.count() << " ms\n";
 
     return 0;
 }
